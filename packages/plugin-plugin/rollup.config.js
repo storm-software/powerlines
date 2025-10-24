a887457aa567ce16c0ad71db923ec239371b0a04
Pat <pat@patsullivan.org>
2025-10-24 04:13:27 +0000

    fix(plugin-plugin): Resolve issue with missing `powerlines` dependency modules

--- packages/plugin-plugin/rollup.config.js
+++ packages/plugin-plugin/rollup.config.js
@@ -1,6 +1,6 @@
 /* -------------------------------------------------------------------
 
-                  ⚡ Storm Software - Powerlines
+                   ⚡ Storm Software - Powerlines
 
  This code was released as part of the Powerlines project. Powerlines
  is maintained by Storm Software under the Apache-2.0 license, and is
@@ -25,6 +25,7 @@ import typescriptPlugin from "rollup-plugin-typescript2";
 
 const externals = [
   "@powerlines/",
+  "powerlines",
   "@storm-software/",
   "@stryke/",
   "@alloy-js/",
