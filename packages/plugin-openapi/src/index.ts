94449d133dd9de55e5793b2c5de2be5e5ec40286
Pat <pat@patsullivan.org>
2025-10-27 19:01:30 +0000

    feat(plugin-openapi): Initial check-in of the `openapi` plugin

---
+++ packages/plugin-openapi/src/index.ts
@@ -0,0 +1,77 @@
+/* -------------------------------------------------------------------
+
+                   ⚡ Storm Software - Powerlines
+
+ This code was released as part of the Powerlines project. Powerlines
+ is maintained by Storm Software under the Apache-2.0 license, and is
+ free for commercial and private use. For more information, please visit
+ our licensing page at https://stormsoftware.com/licenses/projects/powerlines.
+
+ Website:                  https://stormsoftware.com
+ Repository:               https://github.com/storm-software/powerlines
+ Documentation:            https://docs.stormsoftware.com/projects/powerlines
+ Contact:                  https://stormsoftware.com/contact
+
+ SPDX-License-Identifier:  Apache-2.0
+
+ ------------------------------------------------------------------- */
+
+import { joinPaths } from "@stryke/path/join-paths";
+import defu from "defu";
+import openapiTS, { astToString } from "openapi-typescript";
+import { Plugin } from "powerlines/types/plugin";
+import {
+  OpenAPIPluginContext,
+  OpenAPIPluginOptions,
+  OpenAPIPluginUserConfig
+} from "./types/plugin";
+
+export * from "./types";
+
+/**
+ * A Powerlines plugin to integrate Open-API for code generation.
+ *
+ * @param options - The plugin options.
+ * @returns A Powerlines plugin instance.
+ */
+export const plugin = <
+  TContext extends OpenAPIPluginContext = OpenAPIPluginContext
+>(
+  options: OpenAPIPluginOptions = {}
+): Plugin<TContext> => {
+  return {
+    name: "openapi",
+    config() {
+      return {
+        openapi: defu(options, {
+          schema: joinPaths(
+            this.workspaceConfig.workspaceRoot,
+            this.config.projectRoot,
+            "schema.yaml"
+          ),
+          outputFile: joinPaths(this.artifactsPath, "openapi.ts"),
+          cwd: joinPaths(
+            this.workspaceConfig.workspaceRoot,
+            this.config.projectRoot
+          ),
+          silent: this.config.logLevel === null
+        })
+      } as Partial<OpenAPIPluginUserConfig>;
+    },
+    async prepare() {
+      const ast = await openapiTS(
+        this.config.openapi.schema,
+        this.config.openapi
+      );
+
+      await this.fs.writeFile(
+        this.config.openapi.outputFile,
+        astToString(ast, {
+          fileName: this.config.openapi.outputFile
+        })
+      );
+    }
+  };
+};
+
+export default plugin;
