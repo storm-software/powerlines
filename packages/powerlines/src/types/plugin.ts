ddc99a8536eba7f6080afacbf7e5dd2f8121c6c5
Pat <pat@patsullivan.org>
2025-12-06 15:35:50 +0000

    fix(powerlines): Remove `MaybePromise` from `applyToEnvironment` to prevent type error

--- packages/powerlines/src/types/plugin.ts
+++ packages/powerlines/src/types/plugin.ts
@@ -391,7 +391,7 @@ export interface Plugin<
    */
   applyToEnvironment?: (
     environment: EnvironmentResolvedConfig
-  ) => MaybePromise<boolean | PluginConfig<any>>;
+  ) => boolean | PluginConfig<any>;
 }
 
 export const PLUGIN_NON_HOOK_FIELDS = [
