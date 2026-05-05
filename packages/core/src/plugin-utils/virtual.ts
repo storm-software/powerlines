b7d1350ebb35be7b3c9149eaaea5e7aa6ad72036
Pat <pat@patsullivan.org>
2026-05-05 05:26:22 +0000

    chore(core): Added `prefixRegex` utility function

--- packages/core/src/plugin-utils/virtual.ts
+++ packages/core/src/plugin-utils/virtual.ts
@@ -16,10 +16,67 @@
 
  ------------------------------------------------------------------- */
 
-import { prefixRegex } from "@rolldown/pluginutils";
+const ESCAPE_REGEX = /[-/\\^$*+?.()|[\]{}]/g;
+function escapeRegex(str: string): string {
+  return str.replace(ESCAPE_REGEX, "\\$&");
+}
 
 export const VIRTUAL_MODULE_PREFIX = "\0";
 export const VIRTUAL_MODULE_PREFIX_REGEX = prefixRegex("\0");
+
+export interface PrefixRegexOptions {
+  /**
+   * Flags for the RegExp.
+   */
+  flags?: string;
+
+  /**
+   * The prefix to match. This will be escaped and used as the prefix in the generated RegExp.
+   */
+  prefix?: string;
+
+  /**
+   * Whether the prefix is optional. If true, the generated RegExp will match both strings that start with the prefix and strings that do not start with the prefix. Default is false.
+   */
+  isPrefixOptional?: boolean;
+}
+
+/**
+ * Constructs a RegExp that matches a value that has the specified prefix. This is useful for plugin hook filters.
+ *
+ * @example
+ * ```ts
+ * import { prefixRegex } from '@rolldown/pluginutils';
+ * const plugin = {
+ *   name: 'plugin',
+ *   resolveId: {
+ *     filter: { id: prefixRegex('foo') },
+ *     handler(id) {} // will only be called for IDs starting with `foo`
+ *   }
+ * }
+ * ```
+ *
+ * @param str - the string to match.
+ * @param options - options for the RegExp.
+ * @return a RegExp that matches the specified string with the specified prefix.
+ */
+export function prefixRegex(
+  str: string,
+  options: PrefixRegexOptions = {}
+): RegExp {
+  const { flags, prefix, isPrefixOptional = false } = options;
+
+  return new RegExp(
+    `^${
+      prefix
+        ? isPrefixOptional
+          ? `(${escapeRegex(prefix)})?`
+          : `${escapeRegex(prefix)}`
+        : ""
+    }${escapeRegex(str)}`,
+    flags
+  );
+}
 
 /**
  * Adds the virtual module prefix to the given ID.
@@ -58,5 +115,8 @@ export function isVirtualModule(id: string): boolean {
  * @returns A regular expression that matches the given ID with the virtual module prefix.
  */
 export function createVirtualPrefixRegex(id: string): RegExp {
-  return prefixRegex(addVirtualPrefix(removeVirtualPrefix(id)));
+  return prefixRegex(removeVirtualPrefix(id), {
+    prefix: VIRTUAL_MODULE_PREFIX,
+    isPrefixOptional: true
+  });
 }
