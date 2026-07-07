c8b992b7c8d47a260a5b513d8ab5a36fc4fea6f4
sullivanpj <pat@patsullivan.org>
2026-07-07 08:47:23 +0000

    chore(monorepo): Update devenv profiles

--- devenv.nix
+++ devenv.nix
@@ -14,4 +14,102 @@
   packages = with pkgs; [
     capnproto
   ];
+
+  profiles = {
+    development.module = {
+      env.ENVIRONMENT = "development";
+      env.NODE_ENV = "development";
+      env.DEBUG = true;
+
+      languages = {
+        nix = {
+          enable = true;
+        };
+      };
+
+      tasks = {
+        "storm:setup:git" = {
+          exec = ''
+            git config commit.gpgsign true
+            git config tag.gpgSign true
+            git config lfs.allowincompletepush true
+            git config init.defaultBranch main
+
+            npm config set provenance true
+          '';
+          before = [
+            "devenv:enterShell"
+            "devenv:enterTest"
+          ];
+          after = [
+            "devenv:files"
+            "devenv:files:cleanup"
+          ];
+        };
+        "storm:setup:install" = {
+          exec = ''
+            pnpm exec storm-git pre-install
+            pnpm install --no-frozen-lockfile
+            update-storm
+            bootstrap
+
+            pnpm exec storm-git prepare
+          '';
+          before = [
+            "devenv:enterShell"
+            "devenv:enterTest"
+          ];
+          after = [
+            "devenv:files"
+            "devenv:files:cleanup"
+            "storm:setup:git"
+          ];
+        };
+      };
+    };
+
+    production.module = {
+      env.ENVIRONMENT = "production";
+      env.NODE_ENV = "production";
+      env.DEBUG = false;
+      env.DEVENV_TUI = false;
+
+      tasks = {
+        "storm:setup:git" = {
+          exec = ''
+            git config commit.gpgsign true
+            git config tag.gpgSign true
+            git config lfs.allowincompletepush true
+            git config init.defaultBranch main
+
+            npm config set provenance true
+          '';
+          before = [
+            "devenv:enterShell"
+            "devenv:enterTest"
+            "storm:setup:install"
+          ];
+          after = [
+            "devenv:files"
+            "devenv:files:cleanup"
+          ];
+        };
+        "storm:setup:install" = {
+          exec = ''
+            pnpm install --frozen-lockfile
+            bootstrap
+          '';
+          before = [
+            "devenv:enterShell"
+            "devenv:enterTest"
+          ];
+          after = [
+            "devenv:files"
+            "devenv:files:cleanup"
+            "storm:setup:git"
+          ];
+        };
+      };
+    };
+  };
 }
